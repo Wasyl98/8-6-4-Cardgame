f8fc517e1ad604b2bfac50dc3b075f8ea3773cfd
Wasyl98 <m.wasiluk98@gmail.com>
2018-05-11 15:09:42 +0200

    created card and deck class

---
+++ src/Card.java
@@ -0,0 +1,19 @@
+public class Card {
+    private int value;
+    private String color;
+
+    public Card(int value, String color) {
+        this.value = value;
+        this.color = color;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+}
