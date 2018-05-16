07646076f7512b23bea9cfbb8751e2cb14bed0a5
Wasyl98 <m.wasiluk98@gmail.com>
2018-05-17 01:58:03 +0200

    constructors and basic methods

--- src/Card.java
+++ src/Card.java
@@ -1,10 +1,31 @@
 public class Card {
-    private int value;
-    private String color;
 
-    public Card(int value, String color) {
-        this.value = value;
-        this.color = color;
+    public final static int SPADES = 0;   // Codes for the 4 suits, plus Joker.
+    public final static int HEARTS = 1;
+    public final static int DIAMONDS = 2;
+    public final static int CLUBS = 3;
+    //public final static int JOKER = 4;
+
+    public final static int ACE = 1;      // Codes for the non-numeric cards.
+    public final static int JACK = 11;    //   Cards 2 through 10 have their
+    public final static int QUEEN = 12;   //   numerical values for their codes.
+    public final static int KING = 13;
+
+    private final int value;
+    private final int suit;
+
+    public Card(int theValue, int theSuit) {
+
+        if(theValue<1 || theValue >13){
+            throw  new IllegalArgumentException("Illegal card value");
+        }
+
+        if(theSuit != SPADES && theSuit != HEARTS && theSuit != DIAMONDS && theSuit != CLUBS){
+            throw new IllegalArgumentException("Illegal card suit");
+        }
+
+        this.value = theValue;
+        this.suit = theSuit;
     }
 
     public int getValue() {
@@ -12,8 +33,8 @@ public class Card {
     }
 
 
-    public String getColor() {
-        return color;
+    public int getSuit() {
+        return suit;
     }
 
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,8 +1,39 @@
-import java.util.ArrayList;
-
 public class Deck {
-    ArrayList<Card> deck = new ArrayList<Card>();
+    private Card[] deck;
+    private int cardsUsed;
 
+    public void buildDeck(){
+        deck = new Card[52];
+        int cardCt = 0;
 
+        for (int suit = 0;suit <= 3;suit++) {
+            for (int value = 1;value <= 13;value++) {
+                deck[cardCt] = new Card(value,suit);
+                cardCt++;
+            }
+        }
 
+    }
+
+    public void shuffleDeck(){
+        for ( int i = deck.length-1; i > 0; i-- ) {
+            int rand = (int)(Math.random()*(i+1));
+            Card temp = deck[i];
+            deck[i] = deck[rand];
+            deck[rand] = temp;
+        }
+        cardsUsed = 0;
+
+    }
+
+    public int cardsLeft(){
+        return deck.length - cardsUsed;
+    }
+
+    public Card dealCard(){
+        if(cardsUsed == deck.length)
+            throw new IllegalStateException("Deck is empty");
+        ++cardsUsed;
+        return deck[cardsUsed-1];
+    }
 }
