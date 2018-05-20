447e0d1f7fe04b2ac0010552db3bacb418e2f4f1
Wasyl98 <m.wasiluk98@gmail.com>
2018-05-20 22:13:57 +0200

    TODO add sorting by value and by suit

--- src/Hand.java
+++ src/Hand.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Hand {
+
+    private ArrayList<Card> hand;
+
+    public Hand(){
+        hand = new ArrayList<Card>();
+    }
+
+    public void clear(){
+        hand.clear();
+    }
+
+    public void add(Card c){
+        if(c == null){
+            throw new NullPointerException("Can't add a null card to a hand");
+        }
+        hand.add(c);
+    }
+
+    public void removeCard(Card c){
+        hand.remove(c);
+    }
+
+    public void removeCard(int position){
+        if (position < 0 || position > hand.size()){
+            throw new IllegalArgumentException("Invalid card position: " + position);
+        }
+        hand.remove(position);
+    }
+
+    public int getCardCount(){
+        return hand.size();
+    }
+
+    public Card getCard(int position){
+        if (position < 0 || position > hand.size()) {
+            throw new IllegalArgumentException("Invalid card position: " + position);
+        }
+        return hand.get(position);
+
+    }
+
 }
